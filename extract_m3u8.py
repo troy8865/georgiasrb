d40bbc779d93899a7c77dcda4ec18052d9c19eb9
MEGA-GROUP <uzagdasan@mail.ru>
2025-02-19 00:01:27 +0400

    Update extract_m3u8.py

--- extract_m3u8.py
+++ extract_m3u8.py
@@ -1,7 +1,6 @@
 import os
 import requests
 import time
-import hashlib
 import random
 import string
 
@@ -16,7 +15,7 @@ output_folder = "output"
 os.makedirs(output_folder, exist_ok=True)
 
 # Dinamik token və parametrlər yaratmaq üçün funksiya
-def generate_dynamic_url(base_url):
+def generate_dynamic_url():
     # Token və digər parametrləri yaradırıq
     accesscode = "fbly"  # Sabit qalır
     tkn = ''.join(random.choices(string.ascii_letters + string.digits, k=22))  # 22 simvol uzunluğunda təsadüfi token
@@ -25,14 +24,8 @@ def generate_dynamic_url(base_url):
     ip = ".".join(map(str, (random.randint(0, 255) for _ in range(4))))  # Təsadüfi IP ünvanı
     
     # Parametrləri URL-ə əlavə edirik
-    dynamic_url = f"{base_url}?accesscode={accesscode}&tkn={tkn}&tms={tms}&hst={hst}&ip={ip}"
+    dynamic_url = f"https://cdn900.canlitv.vip/sabantv.m3u8?accesscode={accesscode}&tkn={tkn}&tms={tms}&hst={hst}&ip={ip}"
     return dynamic_url
-
-# .ts linkini əvəz edən funksiya
-def replace_ts_with_m3u8(line, base_url):
-    if line.strip().endswith('.ts'):  # Tam URL və ya nisbi yol olan .ts linklərini tapır
-        return generate_dynamic_url(base_url)
-    return line
 
 # m3u8 faylını çıxar və qovluğa yadda saxla
 def extract_m3u8(url, index):
@@ -52,19 +45,12 @@ def extract_m3u8(url, index):
         print("Qaynaq faylının məzmunu:")
         print(m3u8_content)
         
+        # Dinamik URL yaradırıq
+        dynamic_url = generate_dynamic_url()
+        
         # Multi-variant m3u8 faylı üçün əsas strukturu yaradırıq
         modified_content = "#EXTM3U\n#EXT-X-VERSION:3\n"
-        
-        # İçindəki linkləri işləyib, onların önündəki əlavə edilən hissəni çıxarırıq
-        for line in m3u8_content:
-            if line.strip() and not line.startswith("#"):  # Tərkibdə "#" olmayan sətirləri seç
-                # Linki əvəz etmək üçün funksiya çağırılır
-                base_url = "https://cdn900.canlitv.vip/sabantv.m3u8"
-                modified_line = replace_ts_with_m3u8(line, base_url)
-                modified_content += f"#EXT-X-STREAM-INF:BANDWIDTH=2085600,RESOLUTION=1280x720\n{modified_line}\n"
-            else:
-                # Əgər sətir meta məlumatdırsa, olduğu kimi saxla
-                modified_content += f"{line}\n"
+        modified_content += f"#EXT-X-STREAM-INF:BANDWIDTH=2085600,RESOLUTION=1280x720\n{dynamic_url}\n"
         
         # Faylı qovluğa yaz (üzərinə yaz)
         with open(file_path, "w", encoding="utf-8") as file:
