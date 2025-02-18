d80f96f426c0e0b3b167551ce4b9f7676cb186c2
MEGA-GROUP <uzagdasan@mail.ru>
2025-02-19 02:35:25 +0400

    Create nowtur1.py

---
+++ ressources/tur/nowtur1.py
@@ -0,0 +1,34 @@
+
+import requests
+import re
+import os
+
+# Qovluq yolu və fayl adı
+output_dir = 'output'
+output_file = 'stream_url.txt'
+
+# Qovluğu yoxlayın və əgər yoxdursa yaradın
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+url = 'https://www.nowtv.com.tr/canli-yayin'
+response = requests.get(url, verify=False)
+
+if response.status_code == 200:
+    match = re.search(r"daiUrl\s*:\s*'(https?://[^\']+)'", response.text)
+  
+    if match:
+        erstrm = match.group(1)
+        print(f"Tapılan URL: {erstrm}")
+        
+        # Fayla yazmaq üçün tam yol
+        file_path = os.path.join(output_dir, output_file)
+        
+        # Fayla yazma əməliyyatı
+        with open(file_path, 'w') as file:
+            file.write(erstrm)
+        print(f"URL '{file_path}' faylına uğurla yazıldı.")
+    else:
+        print("erstrm not found in the content.")
+else:
+    print(f"Failed to fetch content. HTTP Status code: {response.status_code}")
