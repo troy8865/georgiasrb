dab200003a59072b7fd79213cea3e713cb996a5d
MEGA-GROUP <uzagdasan@mail.ru>
2025-03-04 01:33:22 +0400

    Update extract_m3u8.py

--- extract_m3u8.py
+++ extract_m3u8.py
@@ -3,7 +3,7 @@ import requests
 
 # Qaynaq linkləri
 source_urls = [
-    "http://raalbatros.serv00.net/Freeshot.php?ID=bein-sports-1-turkey/158",
+    "http://php-yayin-paylasim.ct.ws/DTH-Justin-V1.php",
     # Buraya digər m3u8 linklərini əlavə edin
 ]
 
